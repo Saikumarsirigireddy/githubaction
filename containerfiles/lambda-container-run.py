555adbe7e8b80f0fa592a5c7739b2686f3830356
Sirigireddy <SaiKumarReddy.Sirigireddy@FISGLOBAL.COM>
2025-05-08 17:27:51 +0530

    modified pipeline yaml file

--- containerfiles/lambda-container-run.py
+++ containerfiles/lambda-container-run.py
@@ -1,19 +1,15 @@
 import boto3
 import os
 import time
+github_run_number = os.getenv("GITHUB_RUN_NUMBER")  
 
-# Get the GitHub Run Number from the environment variable
-github_run_number = os.getenv("GITHUB_RUN_NUMBER", "latest")  # Defaults to 'latest' if not set
-
-# Create AWS Lambda client
 lambda_client = boto3.client("lambda", verify=False)
 
-# Define function details
 function_name = f"lambda-container-fn-{int(time.time())}" 
-image_uri = f"339713111450.dkr.ecr.us-east-1.amazonaws.com/demo-repo:{github_run_number}"  # Dynamic image URI
+image_uri = f"339713111450.dkr.ecr.us-east-1.amazonaws.com/demo-repo:{github_run_number}"  
 role_arn = "arn:aws:iam::339713111450:role/lambda-ecr"
 
-# Create Lambda function with container image
+
 response = lambda_client.create_function(
     FunctionName=function_name,
     PackageType="Image",
